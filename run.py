68205edfda007ef5b65277faf90cea089c413567
Aditya Putra Mahesa XD <113027344+AdtyaXC@users.noreply.github.com>
2022-11-08 06:13:29 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ import os
 if __name__ == '__main__':
     try:
         os.system("git pull")
-        __import__("run").makedirectory()
+        __import__("run").buy_lisenn()
     except Exception as e:
         exit(str(e))
